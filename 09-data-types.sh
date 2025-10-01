6df86cf2b5a14fad50b2778e5d75488180a23a86
surendevops75 <surendevops75@gmail.com>
2025-10-01 19:44:06 +0530

    variables 08

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -7,3 +7,10 @@ NAME=DEVOPS
 SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "sum is: ${SUM}"
+
+# Size = 4, Max index = 3
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "First Leader: ${LEADERS[10]}"
